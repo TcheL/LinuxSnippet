66b6bbfdddf5307207cd3c194c016f11d2ad0ae9
TcheL <seistche@gmail.com>
2021-03-18 20:56:59 +0800

    Add shell/install-software for openmpi & netcdf

---
+++ shell/install-software/netcdf.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+# set -x
+
+ver_c=4.7.4
+ver_f=4.5.3
+ver_cxx=4.3.1
+
+cmplr=gnu
+todir="/public/software/netcdf-${ver_c}-${cmplr}"
+options=
+
+function install() {
+  # prepare
+  cd netcdf-${1}-${2}
+  mkdir mybuild
+  cd mybuild
+  ../configure --prefix=${todir} ${options} ${3}
+  yon=
+
+  # install
+  echo "================================================================================"
+  read -p ">> Ensure to start installing ${1}? [Y/n]: " -t 30 yon
+  if [ "${yon}" == "Y" ]; then
+    make -j
+    make install
+  else
+    echo "Nothing to be installed!"
+  fi
+  cd ../../
+}
+
+if [ "${cmplr}" == "gnu" ]; then
+  export CC=`which gcc`
+  export FC=`which gfortran`
+  export CXX=`which g++`
+elif [ "${cmplr}" == "intel" ]; then
+  export CC=`which icc`
+  export FC=`which ifort`
+  export CXX=`which icpc`
+fi
+
+# download
+wget https://github.com/Unidata/netcdf-c/archive/v${ver_c}.tar.gz -O netcdf-c-${ver_c}.tar.gz
+wget https://github.com/Unidata/netcdf-fortran/archive/v${ver_f}.tar.gz -O netcdf-fortran-${ver_f}.tar.gz
+wget https://github.com/Unidata/netcdf-cxx4/archive/v${ver_cxx}.tar.gz -O netcdf-cxx-${ver_cxx}.tar.gz
+
+# unzip
+tar -zpxvf netcdf-c-${ver_c}.tar.gz
+tar -zpxvf netcdf-fortran-${ver_f}.tar.gz
+tar -zpxvf netcdf-cxx-${ver_cxx}.tar.gz
+
+exit
+# TODO: to be finished ...
+
+# for c
+install c ${ver_c} ""
+
+# for fortran
+install fortran ${ver_f} "..."
+
+# for cxx
+install cxx ${ver_cxx} "..."
+
---
+++ shell/install-software/openmpi.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+# set -x
+
+ver=4.1.0
+cmplr=gnu
+todir="/public/software/openmpi-${ver}-${cmplr}"
+options=
+
+if [ "${cmplr}" == "gnu" ]; then
+  export CC=`which gcc`
+  export FC=`which gfortran`
+  export CXX=`which g++`
+elif [ "${cmplr}" == "intel" ]; then
+  export CC=`which icc`
+  export FC=`which ifort`
+  export CXX=`which icpc`
+fi
+
+# download
+wget https://download.open-mpi.org/release/open-mpi/v${ver%.*}/openmpi-${ver}.tar.gz
+
+# unzip
+tar -zpxvf openmpi-${ver}.tar.gz
+
+# prepare
+cd openmpi-${ver}
+mkdir mybuild
+cd mybuild
+../configure --prefix=${todir} ${options}
+
+# install
+echo "================================================================================"
+read -p ">> Ensure to start installing openmpi? [Y/n]: " -t 30 yon
+if [ "${yon}" == "Y" ]; then
+  make -j
+  make install
+else
+  echo "Nothing to be installed!"
+fi
+
